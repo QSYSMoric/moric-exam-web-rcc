bad7b7badb7b3c1066adf56e2acbdf06ac6cc357
moric <183957330@qq.com>
2025-05-20 10:57:56 +0800

    ✨ feat: 初始化

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,7 @@
 import {
     defineConfig,
-    presetAttributify,
-    presetIcons,
     presetTypography,
     presetUno,
-    presetWebFonts,
     transformerDirectives,
     transformerVariantGroup,
 } from 'unocss';
@@ -22,19 +19,7 @@ export default defineConfig({
     ],
     presets: [
         presetUno(),
-        presetAttributify(),
-        presetIcons({
-            scale: 1.2,
-            warn: true,
-        }),
         presetTypography(),
-        presetWebFonts({
-            fonts: {
-                sans: 'DM Sans',
-                serif: 'DM Serif Display',
-                mono: 'DM Mono',
-            },
-        }),
     ],
     transformers: [transformerDirectives(), transformerVariantGroup()],
 });
